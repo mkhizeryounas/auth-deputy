098ef5106ff47ac5690a31c0491051b407cf8b93
Khizer Younas <m.khizeryounas@gmail.com>
2019-12-16 00:33:57 +0500

    docs: added example env

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV === "production") require("dotenv").config();
 module.exports = {
-  mongodb: process.env.mongodb || "mongodb://localhost:27017/authdeputy",
+  mongodb:
+    process.env.mongodb ||
+    "mongodb://mkhizeryounas:Mkhizer_321@ds255728.mlab.com:55728/authdeputy",
   secret: process.env.secret || "c6aSsUzQBACrdWoWy6g7BkuxwKfkPbmB"
 };
--- src/modules/locker.js
+++ src/modules/locker.js
@@ -4,57 +4,59 @@ const { Realm } = require("../../config/models");
 
 let data = {
   unlock: (required_scope = null) => async (request, response, next) => {
-    let realmConfig = await Realm.findOne();
-    if (!realmConfig) {
-      throw {
-        statusCode: 401,
-        data: {
-          message: `No realm exists`
-        }
-      };
-    }
-    realmConfig = realmConfig.toJSON();
-    let authHeader = request.headers["authorization"] || "";
-    if (typeof authHeader !== "undefined" && authHeader.includes("Bearer ")) {
-      authHeader = authHeader.substring(7);
-      jwt.verify(
-        authHeader,
-        realmConfig.public_key,
-        { algorithm: realmConfig.algorithm },
-        (err, decode) => {
-          try {
-            if (err) throw authHeader;
-            let authZ = false;
-            if (required_scope) {
-              decode.scopes.split(",").map(e => {
-                if (required_scope === e) {
-                  authZ = true;
-                  return false;
-                }
-              });
-            } else {
-              authZ = true;
-            }
-            if (!authZ) {
-              throw {
-                statusCode: 403,
-                data: {
-                  message: `[SCOPE ERROR] - This route requires '${required_scope}' scope.`
-                }
-              };
-            }
-            request.user = decode;
-            next();
-          } catch (error) {
-            response.reply({
-              statusCode: error.statusCode || 401,
-              data: error.data
-            });
+    try {
+      let realmConfig = await Realm.findOne().then(e => e.toJSON());
+      if (!realmConfig)
+        throw {
+          statusCode: 401,
+          data: {
+            message: `No realm exists`
           }
-        }
-      );
-    } else {
-      response.reply({ statusCode: 401 });
+        };
+      let authHeader = request.headers["authorization"] || "";
+      if (typeof authHeader !== "undefined" && authHeader.includes("Bearer ")) {
+        authHeader = authHeader.substring(7);
+        jwt.verify(
+          authHeader,
+          realmConfig.public_key,
+          { algorithm: realmConfig.algorithm },
+          (err, decode) => {
+            try {
+              if (err) throw authHeader;
+              let authZ = false;
+              if (required_scope) {
+                decode.scopes.split(",").map(e => {
+                  if (required_scope === e) {
+                    authZ = true;
+                    return false;
+                  }
+                });
+              } else {
+                authZ = true;
+              }
+              if (!authZ) {
+                throw {
+                  statusCode: 403,
+                  data: {
+                    message: `[SCOPE ERROR] - This route requires '${required_scope}' scope.`
+                  }
+                };
+              }
+              request.user = decode;
+              next();
+            } catch (error) {
+              response.reply({
+                statusCode: error.statusCode || 401,
+                data: error.data
+              });
+            }
+          }
+        );
+      } else {
+        response.reply({ statusCode: 401 });
+      }
+    } catch (err) {
+      response.reply({ statusCode: err.status || 401 });
     }
   },
   lock: async (obj, offline_flag = false) => {
